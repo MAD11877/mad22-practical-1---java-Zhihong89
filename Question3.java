97ff10ed6b9e87cf05020caa9dca44f86029f245
Zhihong89 <104065056+Zhihong89@users.noreply.github.com>
2022-04-24 23:47:02 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -12,5 +12,14 @@ public class Question3
      
     Scanner in = new Scanner(System.in);
     
+    int i = in.nextInt();
+
+    for (int v=i; v>0; v--) 
+    {
+      for (int k=0; k<v; k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
